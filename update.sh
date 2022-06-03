b6c1576b70c8d04f33c5da8670c753602d7617ad
blacksky3 <blacksky3@tuta.io>
2022-06-02 20:25:44 -0400

    add update script

---
+++ update.sh
@@ -0,0 +1,20 @@
+# _     _            _        _          _____
+#| |__ | | __ _  ___| | _____| | ___   _|___ /
+#| '_ \| |/ _` |/ __| |/ / __| |/ / | | | |_ \
+#| |_) | | (_| | (__|   <\__ \   <| |_| |___) |
+#|_.__/|_|\__,_|\___|_|\_\___/_|\_\\__, |____/
+#                                  |___/
+
+#Maintainer: blacksky3 <blacksky3@tuta.io> <https://github.com/blacksky3>
+
+#!/bin/bash
+
+source=$(pwd)
+
+echo "${source}"
+
+# updpkgsums
+
+cd linux515 && updpkgsums && cd ${source}
+
+cd linux518 && updpkgsums && cd ${source}
